f4f53e06d8dc301bb070f9d958a5383477e2e1bd
prashantsingh9079 <prashant.singh9079@gmail.com>
2023-06-26 11:03:48 +0530

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import {Nav,Card,Button} from "react-bootstrap"
 import './App.css';
+import alb1 from '../src/assets/album1.jpeg'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Nav style={{background:'black',border:'2px',borderColor:'white'}} className="justify-content-center" activeKey="/home">
+        <Nav.Item>
+          <Nav.Link style={{color:'white'}} href="/home">HOME</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link style={{color:'white'}} eventKey="link-1">STORE</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link style={{color:'white'}} eventKey="link-2">ABOUT</Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <Card style={{ width: '98rem',textAlign:'center',height:'12rem', fontFamily:'fantasy', fontSize:'xx-large',color:'white',background:'grey' }}>
+      <Card.Body>
+        <Card.Title>THE GENERICS</Card.Title>
+      </Card.Body>
+    </Card>
+    <div style={{textAlign:'center'}}>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={alb1} />
+      <Card.Body>
+        <Card.Title>Album 1</Card.Title>
+        
+        <Button variant="primary">Add to Cart</Button>
+      </Card.Body>
+    </Card>
+
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={alb1} />
+      <Card.Body>
+        <Card.Title>Album 2</Card.Title>
+        
+        <Button variant="primary">Add to Cart</Button>
+      </Card.Body>
+    </Card>
+
     </div>
+    </>
   );
 }
 
